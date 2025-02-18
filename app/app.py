2ede4f8508bbe6b2fea512a8ef140b37da8e9c4d
khris-xp <khrisbharmmano@gmail.com>
2025-02-18 15:31:06 +0700

    fix: logic plus module

--- app/app.py
+++ app/app.py
@@ -5,16 +5,27 @@ app = Flask(__name__)
 
 @app.route("/getcode", methods=["GET"])
 def getcode():
-    return "Hi my name is Khris Bharmmano"
+    return "Hi my name is khris bharmmano"
 
 
 @app.route("/plus/<num1>/<num2>", methods=["GET"])
 def plus(num1, num2):
-    try:
-        result = float(num1) + float(num2)
-        return jsonify({"result": int(result) if result.is_integer() else result}), 200
-    except ValueError:
-        return jsonify({"error_msg": "Inputs must be valid numbers"}), 400
+    with app.app_context():
+        try:
+            num1 = float(num1)
+            num2 = float(num2)
+
+            ans = num1 + num2
+
+            if ans.is_integer():
+                ans = int(ans)
+
+            result = {"result": ans}
+            return jsonify(result), 200
+
+        except ValueError:
+            result = {"error_msg": "Inputs must be valid numbers"}
+            return jsonify(result), 400
 
 
 if __name__ == "__main__":
