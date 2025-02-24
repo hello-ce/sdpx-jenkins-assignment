ae7a72e55aefae837bc4e5721a57de1bbaae82f2
khris-xp <khrisbharmmano@gmail.com>
2025-02-24 11:09:01 +0700

    fix: practice test

--- app/app.py
+++ app/app.py
@@ -8,44 +8,25 @@ def getcode():
     return "Hi my name is khris bharmmano"
 
 
-@app.route("/plus/<num1>/<num2>", methods=["GET"])
-def plus(num1, num2):
-    with app.app_context():
-        try:
-            num1 = float(num1)
-            num2 = float(num2)
-
-            print("Hello World!")
-
-            ans = num1 + num2
-
-            if ans.is_integer():
-                ans = int(ans)
-
-            result = {"result": ans}
-            return jsonify(result), 200
-
-        except ValueError:
-            result = {"error_msg": "Inputs must be valid numbers"}
-            return jsonify(result), 400
-
-
 @app.route("/is_prime/<num1>", methods=["GET"])
 def is_prime(num1):
     with app.app_context():
         try:
-            num1 = int(num1)
-            if num1 <= 1:
-                return jsonify({"error_msg": "Number must be greater than 1"}), 400
+            num = int(num1)
+            if num < 2:
+                result = {"result": False}
+                return jsonify(result), 200
+            for i in range(2, int(num**0.5) + 1):
+                if num % i == 0:
+                    result = {"result": False}
+                    return jsonify(result), 200
 
-            for i in range(2, num1):
-                if num1 % i == 0:
-                    return jsonify({"result": False}), 200
-
-            return jsonify({"result": True}), 200
+            result = {"result": True}
+            return jsonify(result), 200
 
         except ValueError:
-            return jsonify({"error_msg": "Input must be a valid integer"}), 400
+            result = {"error_msg": "Input must be a valid integer"}
+            return jsonify(result), 400
 
 
 if __name__ == "__main__":
